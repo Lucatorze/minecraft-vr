f689bd52ed5d145f1f127b48122521f16134e023
Glitch (minecraft-vr) <none>
2018-02-28 14:31:12 +0000

    🌞👲 Checkpoint views/index.html:704499/33 js/move_camera_forward.js:704499/5 js/inventory.js:704499/1

---
+++ js/inventory.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('inventory', {
+  schema: {
+    speed: {type: 'float', default :0.1},
+  },
+
+  tick: function () {
+    var currentPos= this.el.getAttribute('position');
+    var sceneEl = document.querySelector('a-scene');
+    var camera = sceneEl.querySelector('a-camera');
+    var cameraDirection = sceneEl.querySelector('a-camera').object3D.getWorldDirection()
+
+    this.el.setAttribute('position', {
+      x: currentPos.x + -(0.05 *cameraDirection.x),
+      y: currentPos.y,
+      z: currentPos.z + -(0.05 *cameraDirection.z)
+    })
+  }
+});
