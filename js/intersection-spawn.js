60e4a1195740dde10376e9e01c24493dcb6de6ad
Glitch (safe-carp) <none>
2018-02-28 10:50:03 +0000

    🏪💧 Checkpoint js/intersection-spawn.js:704499/278

--- js/intersection-spawn.js
+++ js/intersection-spawn.js
@@ -3,35 +3,35 @@ AFRAME.registerComponent('intersection-spawn', {
     default: '',
     parse: AFRAME.utils.styleParser.parse
   },
-
+    
   init: function () {
     const data = this.data;
     const el = this.el;
-    var sceneEl = document.querySelector('a-scene');
-    var cursor = sceneEl.querySelector('a-cursor')
-    var verif = cursor.getAttribute('intersection-spawn')
     
-  
-    el.addEventListener(data.event, evt => {
-      if(verif != null){
-      console.log(verif)
-    }else{
-      console.log('nullllllll')
-    }
-      // Create element.
-      const spawnEl = document.createElement('a-entity');
-    
-      // Snap intersection point to grid and offset from center.
-      spawnEl.setAttribute('position', evt.detail.intersection.point);
-
-      // Set components and properties.
-      Object.keys(data).forEach(name => {
-        if (name === 'event') { return; }
-        AFRAME.utils.entity.setComponentProperty(spawnEl, name, data[name]);
-      });
-      console.log(el.sceneEl)
-      // Append to scene.
-      el.sceneEl.appendChild(spawnEl);
-    });
+    el.addEventListener(data.event, callback.bind(this, data, el));
+  },
+    remove:function(){
+      const data = this.data;
+      const el = this.el;
+      console.log('removed')
+      el.removeEventListener(data.event, callback.bind(this, data, el));
   }
 });
+  
+function callback (data,el,evt){
+  var sceneEl = document.querySelector('a-scene');
+  // Create element.
+  const spawnEl = document.createElement('a-entity');
+
+  // Snap intersection point to grid and offset from center.
+  spawnEl.setAttribute('position', evt.detail.intersection.point);
+
+  // Set components and properties.
+  Object.keys(data).forEach(name => {
+    if (name === 'event') { return; }
+    AFRAME.utils.entity.setComponentProperty(spawnEl, name, data[name]);
+  });
+  console.log(el.sceneEl)
+  // Append to scene.
+  el.sceneEl.appendChild(spawnEl);
+}
