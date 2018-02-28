ad1fdeaf79d73be98a02ab1077a83669ef98e02d
Glitch (safe-carp) <none>
2018-02-28 13:19:48 +0000

    👋🍘 Checkpoint views/index.html:704499/5 js/infinite-foward.js:704499/189

--- js/infinite-foward.js
+++ js/infinite-foward.js
@@ -8,11 +8,29 @@ AFRAME.registerComponent('inifite-foward', {
     var sceneEl = document.querySelector('a-scene');
     var cursor = sceneEl.querySelector('a-cursor');
     var camera = sceneEl.querySelector('a-camera');
-    console.log(cursor.getAttribute());
+    var camRotation = camera.getAttribute('rotation');
+    var new_x = 0;
+    var new_z = 0;
+    console.log(camRotation)
     this.el.setAttribute('position', {
-      x: currentPos.x,
+      x: new_x + (0.1 * Math.sin(radian)),
       y: currentPos.y,
-      z: currentPos.z - this.data.speed,
+      z: (new_z + (0.1 * Math.cos(camRotation.y)))
     })
   }
 });
+
+    var new_x = 0;
+    var new_z = 0;
+    function move_camera_forward() {
+        x = $("#cam").attr("position").x;
+        y = $("#cam").attr("position").y;
+        z = $("#cam").attr("position").z;
+        radian = -($("#cam").attr("rotation").y) * (Math.PI / 180);
+        new_z = (new_z + (0.1 * Math.cos(radian)));
+        new_x = new_x + (0.1 * Math.sin(radian));
+        new_pos = new_x + " " + y + " " + (-new_z);
+        console.log(new_pos)
+        $("#cam").attr("position", new_pos)
+
+    }
