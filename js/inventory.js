fa16cc8ffbe29e58a96dbb793a40cc6794badb89
Glitch (minecraft-vr) <none>
2018-03-01 14:56:44 +0000

    🔋🎆 Checkpoint js/inventory.js:704499/86

--- js/inventory.js
+++ js/inventory.js
@@ -1,9 +1,6 @@
 AFRAME.registerComponent('inventory', {
   schema: {
     speed: {type: 'float', default :0.05},
-    dx : {type: 'float'},
-    dy : {type: 'float'},
-    dz : {type: 'float'},
   },
 
   tick: function () {
@@ -14,17 +11,21 @@ AFRAME.registerComponent('inventory', {
     var rotationCam = camera.getAttribute('rotation');
     var rotationEl = this.el.getAttribute('rotation');
     var pos = this.el.getAttribute('position')
-    
     var menu = this.el.object3D;
+    
+    var dx = currentPos.x - (pos.x)
+    var dy = currentPos.y - (pos.y)
+    var dz = currentPos.z - (pos.z)
+    
     let newPosDict = {
         x: pos.x + this.data.speed * Math.sin((rotationCam.y - 180) * (Math.PI / 180)),
         y: pos.y + this.data.speed * Math.tan((rotationCam.x - 180) * (Math.PI / 180)),
         z: pos.z + this.data.speed * Math.cos((rotationCam.y - 180) * (Math.PI / 180)),
     }
     this.el.setAttribute('position', {
-      x: currentPos.x + this.data.dx * Math.sin(((rotationCam.y + rotationEl.y)  - 180) * (Math.PI / 180)),
-      y: currentPos.y + this.data.dy,
-      z: currentPos.z + this.data.dz * Math.cos(((rotationCam.y + rotationEl.y)  - 180) * (Math.PI / 180)),
+      x: currentPos.x + dx * Math.sin(((rotationCam.y + rotationEl.y)  - 180) * (Math.PI / 180)),
+      y: currentPos.y + dy,
+      z: currentPos.z + dz * Math.cos(((rotationCam.y + rotationEl.y)  - 180) * (Math.PI / 180)),
     })
     
   }
