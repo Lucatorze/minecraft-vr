f0c444a88469be95c4c578468f62cdc6634dac4e
Glitch (minecraft-vr) <none>
2018-03-02 13:19:49 +0000

    🍖📍 Checkpoint views/index.html:704499/112 js/menu.js:704499/60

--- js/menu.js
+++ js/menu.js
@@ -120,3 +120,31 @@ AFRAME.registerComponent('edit', {
     });
   }
 });
+
+//CHANGE MOD
+
+AFRAME.registerComponent('change', {
+  dependencies: ['material'],
+
+  init: function () {
+    var el = this.el;  // <a-box>
+    var sceneEl = document.querySelector('a-scene');
+    var cursor = sceneEl.querySelector('a-cursor');
+    var camera = sceneEl.querySelector('a-camera');
+    var menu = sceneEl.querySelector('#menu');
+    var timer;
+    
+    el.addEventListener('mouseenter', function () {
+      timer = setTimeout(function(){
+            console.log('Edit mod')
+            cursor.setAttribute('random-texture', '');
+          camera.removeAttribute('move_camera_forward');
+          camera.removeAttribute('fly_forward');
+          menu.removeAttribute('menu');
+      }, 1000);
+    })
+    el.addEventListener('mouseleave', function () {
+        clearTimeout(timer);
+    });
+  }
+});
